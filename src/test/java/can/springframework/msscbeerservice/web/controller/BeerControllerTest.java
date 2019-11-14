f3ec57437c5fbf84540ca31ab417a1f5ae002d6a
canadmin <icanyard@gmail.com>
2019-11-14 03:10:38 +0300

    fixed tests

--- src/test/java/can/springframework/msscbeerservice/web/controller/BeerControllerTest.java
+++ src/test/java/can/springframework/msscbeerservice/web/controller/BeerControllerTest.java
@@ -1,6 +1,8 @@
 package can.springframework.msscbeerservice.web.controller;
 
+import can.springframework.msscbeerservice.domain.Beer;
 import can.springframework.msscbeerservice.web.model.BeerDto;
+import can.springframework.msscbeerservice.web.model.BeerStyleEnum;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
@@ -10,6 +12,7 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
+import java.math.BigDecimal;
 import java.util.UUID;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
@@ -32,7 +35,7 @@ class BeerControllerTest {
 
     @Test
     void saveNewBeer() throws Exception {
-        BeerDto beerDto =BeerDto.builder().build();
+        BeerDto beerDto = getValidBeerDto();
         String beerDtoJson = objectMapper.writeValueAsString(beerDto); // beerDto'yu json olarak çevirdi
 
         mockMvc.perform(post("/api/v1/beer/")
@@ -42,12 +45,18 @@ class BeerControllerTest {
 
     @Test
     void updateBeerById() throws Exception {
-        BeerDto beerDto =BeerDto.builder().build();
+        BeerDto beerDto = getValidBeerDto();
         String beerDtoJson = objectMapper.writeValueAsString(beerDto);
 
         mockMvc.perform(put("/api/v1/beer/"+UUID.randomUUID().toString())
         .contentType(MediaType.APPLICATION_JSON)
         .content(beerDtoJson))
                 .andExpect(status().isNoContent());
+    }
+
+    BeerDto getValidBeerDto(){
+        return BeerDto.builder().beerName("My Beer").beerStyleEnum(BeerStyleEnum.ALE)
+                .price(new BigDecimal("2.33"))
+                .upc(1234567891322L).build();
     }
 }
